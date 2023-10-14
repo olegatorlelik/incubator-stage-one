dddd6db48c5e20ecc16b9cd4412eeb3ecdd2e2de
OlegBelokonev <lelik.belokonevvv@gmail.com>
2023-10-14 13:20:13 +0200

    fix: port application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-import express from 'express'
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const jsonBodyParser = bodyParser.json();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening port: ${port}`);
+});
