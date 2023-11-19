7d7e2c5299ed0525fe859e3caa09c7eac60f19b5
OlegBelokonev <lelik.belokonevvv@gmail.com>
2023-11-19 14:38:56 +0100

    feat: adding ci cd

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ docker stop incubator-stage-one
 docker rm incubator-stage-one
 docker rmi incubator-stage-one_image
 docker build -t incubator-stage-one_image ~/incubator-stage-one
-docker run --restart unless-stopped --name incubator-stage-one -p 3005:3000 -d incubator-stage-one_image
+docker run --restart unless-stopped --name incubator-stage-one -p 5050:3000 -d incubator-stage-one_image
