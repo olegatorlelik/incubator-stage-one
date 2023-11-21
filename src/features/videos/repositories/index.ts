8ba2c83168b7a5d35a6d05c066e1a831164a2736
OlegBelokonev <lelik.belokonevvv@gmail.com>
2023-11-21 11:35:29 +0100

    fix: remove endpoint

--- src/features/videos/repositories/index.ts
+++ src/features/videos/repositories/index.ts
@@ -15,7 +15,6 @@ class VideoRepository {
    */
   public getVideoById = (id: string): IVideoView | void => {
     const videos = db.getData('videos');
-
     const video = videos.find(({ id: vId }) => Number(id) === vId);
 
     if (!video) {
